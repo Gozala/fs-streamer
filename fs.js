d3f7bccd22d99cdb0951122f21f1a34fdb0a2fee
Irakli Gozalishvili <rfobic@gmail.com>
2011-08-04 18:08:29 +0200

    Initial version.

---
+++ fs.js
@@ -0,0 +1,44 @@
+/* vim:set ts=2 sw=2 sts=2 expandtab */
+/*jshint asi: true undef: true es5: true node: true devel: true
+         forin: true latedef: false supernew: true */
+/*global define: true exports: true */
+
+"use strict";
+
+var fs = require('fs')
+
+exports.list = function list(path) {
+  return function stream(next, stop) {
+    fs.readdir(path, function callback(error, entries, entry) {
+      if (!error) while ((entry = entries.shift())) next(entry)
+      stop(error)
+    })
+  }
+}
+
+exports.readFile = function readFile(path, encoding) {
+  var args = Array.prototype.slice.call(arguments)
+  return function stream(next, stop) {
+    fs.readFile.apply(null, args.concat([function callback(error, data) {
+      if (!error) next(data)
+      stop(error)
+    }]))
+  }
+}
+
+exports.writeFile = function writeFile(path, data, encoding) {
+  var args = Array.prototype.slice.call(arguments)
+  return function stream(next, stop) {
+    fs.writeFile.apply(null, args.concat([ stop ]))
+  }
+}
+
+exports.stat = function stat(path) {
+  return function stream(next, stop) {
+    var descriptor = { path: { value: path, enumerable: true } }
+    fs.stat(path, function callback(error, stats) {
+      if (!error) next(Object.create(stats, descriptor))
+      stop(error)
+    })
+  }
+}
